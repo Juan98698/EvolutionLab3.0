b1df23e7dc1a8434ebb08e9a79f944c5f88d92d2
JUANCARDONA98 <juancardona321829@correo.itm.edu.co>
2026-05-27 14:53:06 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -15,7 +15,6 @@ const STATIC_ASSETS = [
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      console.log('[Service Worker] Precaching static assets');
       return cache.addAll(STATIC_ASSETS);
     }).then(() => self.skipWaiting())
   );
@@ -28,7 +27,6 @@ self.addEventListener('activate', event => {
       return Promise.all(
         keys.map(key => {
           if (key !== CACHE_NAME) {
-            console.log('[Service Worker] Removing old cache', key);
             return caches.delete(key);
           }
         })
